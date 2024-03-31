66a8c0f7e5f771d7820c6600be3d6367180443db
cht03 <3318303250@QQ.com>
2024-03-31 17:01:18 +0800

    1

--- JiaSuDuGongLvPuMiDu.m
+++ JiaSuDuGongLvPuMiDu.m
@@ -3,7 +3,7 @@ n = 0.011:0.01:2.83; % 从0.011到2.83，步长为0.01
 n0 = 0.1;
 Gq0 = 2.56e-4;
 u = 20; % m/s
-w0=10;
+w0 = 10; % 请根据实际情况修改
 
 % 初始化数组
 f = zeros(size(n));
@@ -11,25 +11,29 @@ w = zeros(size(n));
 Gqf = zeros(size(n));
 PLB = zeros(size(n));
 PLB2 = zeros(size(n));
-H=zeros(size(n));
-Gz=zeros(size(n));
+H = zeros(size(n));
+Gz = zeros(size(n));
 
-% 循环计算每个n对应的f, Gqf, Gq1f和Gq2f
+% 循环计算每个n对应的f, w, PLB, PLB2, H, Gqf和Gz
 for i = 1:length(n)
     f(i) = u * n(i);
     w(i) = 2*pi*f(i);
-    PLB(i)=w(i)/w0;
-    PLB2(i) =(w(i)/w0)^2;
+    PLB(i) = w(i) / w0;
+    PLB2(i) = (w(i) / w0)^2;
     ff = f(i) * f(i);
     Gqf(i) = Gq0 * n0 * n0 * u * (1 / ff);
-    H(i)=w(i)*w(i)*sqrt((1+PLB2(i))/((1-PLB2(i))*(1-PLB2(i))+PLB2(i)));
-    Gz(i)=H(i)*H(i)*Gqf(i);
+    H(i) = w(i) * w(i) * sqrt((1 + PLB2(i)) / ((1 - PLB2(i)) * (1 - PLB2(i)) + PLB2(i)));
+    Gz(i) = H(i) * H(i) * Gqf(i);
 end
-% 定义Gz2作为n的函数
-Gz2_fun = @(n) H(n)*H(n)*Gqf(n);
 
-% 对Gz2进行积分
-integral_Gz2 = integral(Gz2_fun, min(n), max(n));
+% 定义Gz2作为n的函数的函数句柄
+Gz2_fun = @(n) H(n) * H(n) * Gqf(n);
+
+% 计算积分
+integral_Gz2 = 0;
+for i = 1:length(n)
+    integral_Gz2 = integral_Gz2 + Gz2_fun(n(i));
+end
 
 % 显示积分结果
 disp(['积分结果为: ' num2str(integral_Gz2)]);
@@ -39,7 +43,7 @@ logf = log10(f);
 logw=log10(w);
 logGqf = log10(Gqf);
 logH = log10(H);
-logGz = log10(Gz);
+logGz = log10(Gz2);
 logPLB=log10(PLB);
 
 % 绘制Gqf的图像
